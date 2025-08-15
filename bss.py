27df03eb96118ae8e85d6fcd7bbf54e05eb4283a
endolith <endolith@gmail.com>
2025-08-15 00:53:37 -0400

    huh limits

--- bss.py
+++ bss.py
@@ -1186,15 +1186,15 @@ if __name__ == "__main__":
                 #   delta_radius=0.5, alpha_radius=0.5,
                 )
 
-    # fs, x = sp.io.wavfile.read(r"family reunion screaming kid.wav")
-    # x = x.astype(np.float64) / np.iinfo(x.dtype).max
-    # duet = Duet(x[:100000], n_sources=2, sample_rate=fs, attenuation_max=1.5,
-    #             delay_max=25,  # microphones 7 inches apart = 23 samples
-    #             assignment_mode="ml",
-    #             # assignment_mode="radius", delta_radius=10, alpha_radius=0.3,
-    #             # (δ_peaks, α_peaks)
-    #             manual_peaks=([-1.53061224,  -1], [0.26470588, 0.9]),
-    #             )
+    fs, x = sp.io.wavfile.read(r"family reunion screaming kid.wav")
+    x = x.astype(np.float64) / np.iinfo(x.dtype).max
+    duet = Duet(x[:100000], n_sources=2, sample_rate=fs, attenuation_max=2,
+                delay_max=10,  # microphones 7 inches apart = 23 samples
+                assignment_mode="ml",
+                # assignment_mode="radius", delta_radius=10, alpha_radius=0.3,
+                # (δ_peaks, α_peaks)
+                manual_peaks=([-1.53061224,  -1], [0.26470588, 0.9]),
+                )
 
     estimates = duet()
     for i in range(duet.n_sources):
