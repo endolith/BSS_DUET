42add607ed67831c8a6a153f81ac08ab40f260ef
endolith <endolith@gmail.com>
2025-08-25 23:40:05 -0400

    kinda works

--- bss.py
+++ bss.py
@@ -1202,8 +1202,9 @@ if __name__ == "__main__":
 
     fs, x = sp.io.wavfile.read(r"test_signals/stereo_mix.wav")
     x = x.astype(np.float64) / np.iinfo(x.dtype).max
-    duet = Duet(x[:100000], n_sources=5, sample_rate=fs, attenuation_max=2,
-                delay_max=1,  # microphones 2 cm apart = 0.9 samples
+    duet = Duet(x[:100000], n_sources=5, sample_rate=fs,
+                attenuation_max=0.2,
+                delay_max=0.8,  # microphones 2 cm apart = 0.9 samples
                 assignment_mode="ml",
                 # assignment_mode="radius", delta_radius=10, alpha_radius=0.3,
                 )
